176400ea604ca6826a6242ecdde497b85f33514d
saidugurlu <ugurlumsaid@gmx.de>
2022-06-27 13:55:52 +0200

    loading employees from database

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
-import express from 'express';
- 
+import express from "express";
+import * as qsql from './qtools/qsql.js';
+
 const app = express();
 const port = 3022;
- 
-app.get('/', (req, res) => {
-    res.send('test api');
+
+app.get("/", (req, res) => {
+  res.send(`
+        <style>
+        li {
+            font-size: 1.5rem;
+        }
+        </style>
+        <h1>Employee API</h1>
+        <ul>
+            <li><a href="http://localhost:${port}/employees">http://localhost:${port}/employees</a> = all employees</li>
+            <li><a href="http://localhost:${port}/employees/23">http://localhost:${port}/employees/23</a> = employee with ID 23</li>
+            <li><a href="http://localhost:${port}/employees-territories/23">http://localhost:${port}/employees-territories/23</a> = territories of employee with ID 23</li>
+        </ul>
+    `);
 });
- 
+
+app.get('/employees', async (req, res) => {
+	const employees = await qsql.getRecordsWithSql('SELECT * FROM Employees');
+	res.send(employees);
+});
+
 app.listen(port, () => {
-    console.log(`Listening on http://localhost:${port}`);
+    console.log(`listening on http://localhost:${port}`);
 });
